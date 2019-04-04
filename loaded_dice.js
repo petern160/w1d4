a2e064d7c3bddf37af41697f8410c037abbd8d3c
petern160 <peternguyen160@gmail.com>
2019-04-04 22:29:59 +0000

    loaded dice and rocket ship

---
+++ loaded_dice.js
@@ -0,0 +1,23 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  var index = 0
+     
+
+  return function() {
+     
+     var result = list[index];
+     index++
+     return result;
+     
+    }
+    
+  
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
+console.log(rollLoadedDie());  // 1
+console.log(rollLoadedDie());  // 6
